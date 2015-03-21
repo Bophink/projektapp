4cf85e462ba77a107a38ba7efb5eff2e5a80949d
emiwes <emiwes@kth.se>
2015-03-21 16:20:09 +0100

    Emil - Ändrade till 6 frågor på desktop, 4 på tablet, 3 på phone.

--- js/carouselCtrl.js
+++ js/carouselCtrl.js
@@ -13,14 +13,21 @@ quizApp.controller('CarouselCtrl', function($scope,quizModel) {
       localQ.position = k+1;
       slide.push(localQ);
 
-      // width > 768px -> laptop & desktop
-      if($(window).width() > 768 ){
+      // width => 992px -> laptop & desktop
+      if($(window).width() >= 992){
         if((k+1)%6 === 0 || (k+1) === quiz.questions.length){
           $scope.slides.push(slide);
           slide = [];
         }
       }
-      // width <= 768px -> tablet & phone
+      //  992 > width => 768 -> tablet
+      else if($(window).width() < 992 && $(window).width() >= 768){
+        if((k+1)%4 === 0 || (k+1) === quiz.questions.length){
+          $scope.slides.push(slide);
+          slide = [];
+        }
+      }
+      // width < 768   -> phone
       else{
         if((k+1)%3 === 0 || (k+1) === quiz.questions.length){
           $scope.slides.push(slide);
