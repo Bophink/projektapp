04f21e06c3b12c4bc67ecd4eba597671fede10ef
Anton Sivertsson <sivertsson.anton@gmail.com>
2015-04-20 12:48:59 +0200

    Anton - Stylear home-page

--- js/headerCtrl.js
+++ js/headerCtrl.js
@@ -1,4 +1,4 @@
-quizApp.controller('headerCtrl', function ($scope, $firebaseObject) {
+quizApp.controller('headerCtrl', function ($scope, $firebaseObject, quizModel) {
 	var location = window.location.hash.split("/");
 	$scope.position = location[0]+"/"+location[1];
 	//tar ej hänsyn till sista paremtern i quiz och track
