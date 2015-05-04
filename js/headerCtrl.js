5bb962da6afe5655c6669a2251d2d47038b6ad52
emiwes <emiwes@kth.se>
2015-05-05 00:32:10 +0200

    Emil - Fixat så att vi får en indicator på vilket quiz vi är inne i, i headern. Quizzets namn ligger centrerat i header när man är inne i ett quiz.

--- js/headerCtrl.js
+++ js/headerCtrl.js
@@ -2,4 +2,11 @@ quizApp.controller('headerCtrl', function ($scope, $firebaseObject, quizModel) {
 	var location = window.location.hash.split("/");
 	$scope.position = location[0]+"/"+location[1];
 	//tar ej hänsyn till sista paremtern i quiz och track
+	// $scope.title = quizModel.Quiz.title;
+
+	if(quizModel.Quiz === undefined){
+		$scope.title = '';
+	}else{
+		$scope.title = quizModel.Quiz.title;
+	}
 });
